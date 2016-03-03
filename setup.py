0b51fdc21a542aa0113d584277dcf9d27f6e9f12
bartolini_ira <bartolini@ira.inaf.it>
2016-03-03 15:14:34 +0100

    fixed bug in beamsize calculation

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
       author = "Marco Bartolini, Simona Righini",
       author_email = "bartolini@ira.inaf.it",
       maintainer = "Marco Bartolini",
-      mainteiner_email =" bartolini@ira.inaf.it",
       license = "License :: OSI Approved :: BSD License",
       url = "http://github.com/flyingfrog81/basie/",
       packages = ["basie", "basie.scanmode"],
@@ -17,6 +16,4 @@ setup(
       package_data = {"basie" : ["schemas/*.ini",
                                  "user_templates/*.txt"]},
       scripts = ["scripts/basie"],
-      install_requires = ["configobj", "validate", "astropy>=1.0", "zodbpickle",
-                          "ZODB", "BTrees"],
      )
